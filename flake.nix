a5d21b2b3a0d0d7fd0e9bf83aa8b4df2924c7eb2
Kian <kiany@thatnameisnotavailable.com>
2024-04-20 10:19:25 -0600

    Fix formating

--- flake.nix
+++ flake.nix
@@ -4,44 +4,51 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     basalt-src = {
-        url= "git+https://gitlab.freedesktop.org/mateosss/basalt.git?submodules=1";
-        flake= false;
-        };
-        flake-utils.url = "github:numtide/flake-utils";
+      url = "git+https://gitlab.freedesktop.org/mateosss/basalt.git?submodules=1";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, basalt-src, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: let pkgs = import nixpkgs { system = system; }; 
-    in {
-    packages.default = pkgs.stdenv.mkDerivation {
-    name = "basalt-monado";
-    src = basalt-src;
-    nativeBuildInputs = with pkgs; [
-    cmake
-    ninja
-    git
-    ];
-    buildInputs = with pkgs; [
-        eigen
-        glew
-        libpng
-        lz4
-        bzip2
-        boost
-        gtest
-        opencv
-        libpng
-        lz4
-        bzip2
-        libuvc
-        fmt
-        python3
-	tbb	
-    ];
-    patches = [
-    ./fix.path
-    ];
-    };
-    }
+  outputs = {
+    self,
+    nixpkgs,
+    basalt-src,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {system = system;};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "basalt-monado";
+          src = basalt-src;
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            git
+          ];
+          buildInputs = with pkgs; [
+            eigen
+            glew
+            libpng
+            lz4
+            bzip2
+            boost
+            gtest
+            opencv
+            libpng
+            lz4
+            bzip2
+            libuvc
+            fmt
+            python3
+            tbb
+          ];
+          patches = [
+            ./fix.path
+          ];
+        };
+      }
     );
 }
